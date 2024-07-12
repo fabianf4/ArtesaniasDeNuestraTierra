778c5682fd0e2368fffc4d65cd9d04048d6d4634
fabian <fabianfa.munoz@gmail.com>
2024-07-11 21:12:56 -0500

    Proyecto inicial

---
+++ src/main/java/co/edu/usa/ArtesaniasDeNuestraTierra/ArtesaniasDeNuestraTierraApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usa.ArtesaniasDeNuestraTierra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtesaniasDeNuestraTierraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtesaniasDeNuestraTierraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/usa/ArtesaniasDeNuestraTierra/ArtesaniasDeNuestraTierraApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usa.ArtesaniasDeNuestraTierra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtesaniasDeNuestraTierraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
