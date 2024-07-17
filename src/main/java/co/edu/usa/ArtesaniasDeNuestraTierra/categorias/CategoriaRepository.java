f15dfd461ef1627caf193b4a5bdd14c43e087364
bernardo.amaya <amayabernardo@gmail.com>
2024-07-17 12:53:18 -0500

    Revert "Revert "Revert "Adición clases Categoria"""
    This reverts commit 2c560408bf4aac0b4bcbf8b43ee262aac4d2183d.

--- src/main/java/co/edu/usa/ArtesaniasDeNuestraTierra/categorias/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.usa.ArtesaniasDeNuestraTierra.categorias;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
-	Categoria findByNombre(String nombre);
-}
