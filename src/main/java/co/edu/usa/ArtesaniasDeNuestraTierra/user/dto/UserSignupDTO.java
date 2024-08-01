d8418c10e1a01f89d9333e6f83e8ced8f53c359d
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-08-01 11:40:04 -0500

    Fix: Quitar campo email que no es necesario

--- src/main/java/co/edu/usa/ArtesaniasDeNuestraTierra/user/dto/UserSignupDTO.java
+++ src/main/java/co/edu/usa/ArtesaniasDeNuestraTierra/user/dto/UserSignupDTO.java
@@ -19,10 +19,6 @@ public class UserSignupDTO {
     @NotBlank(message = "El teléfono no puede estar vacío")
     private String phone;
 
-    @NotBlank(message = "El email no puede estar vacío")
-    @Email(message = "El email debe ser válido")
-    private String email;
-
     @NotBlank(message = "La contraseña no puede estar vacía")
     private String password;
 }
