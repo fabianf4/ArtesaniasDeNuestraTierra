43638077a45469e6eddaffedeb631f445a3010bf
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-08-10 12:49:45 -0500

    Inyeccion de repositorios

--- src/main/java/co/edu/usa/ArtesaniasDeNuestraTierra/category/CategoryRepository.java
+++ src/main/java/co/edu/usa/ArtesaniasDeNuestraTierra/category/CategoryRepository.java
@@ -1,7 +1,9 @@
 package co.edu.usa.ArtesaniasDeNuestraTierra.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer>{
 
 }
--- src/main/java/co/edu/usa/ArtesaniasDeNuestraTierra/comments/CommentRepository.java
+++ src/main/java/co/edu/usa/ArtesaniasDeNuestraTierra/comments/CommentRepository.java
@@ -1,7 +1,9 @@
 package co.edu.usa.ArtesaniasDeNuestraTierra.comments;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer>{
 
 }
